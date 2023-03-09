ddec56bef48a8f2fb918d1cb8ff0f4730ad1926d
Mike Rohner <mrohner94@hotmail.com>
2023-03-08 22:14:16 -0500

    for loop, blank var

---
+++ src/fem-intro-to-go/03_basic_syntax/code/exercise_3a.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	mySentence := "Hello this is a sentence."
+	anotherSentenceButSkipIndex := "The purpose of this is to skip index in for loop"
+
+	for index, value := range mySentence {
+		if index%2 == 0 {
+			fmt.Println(string(value))
+		}
+	}
+
+	for _, value := range anotherSentenceButSkipIndex {
+		fmt.Println(value)
+	}
+}
