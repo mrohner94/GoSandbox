a99dae27ffc100b1800314b3d6b4f62d32b360b4
Mike Rohner <mrohner94@hotmail.com>
2023-03-08 22:34:01 -0500

    functions exercise

--- src/fem-intro-to-go/04_complex_structures/code/functions.go
+++ src/fem-intro-to-go/04_complex_structures/code/functions.go
@@ -1,15 +1,23 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// func printAge(age1, age2 int) (ageOfSally, ageOfBob int) {
-// 	ageOfSally = age1
-// 	ageOfBob = age2
-// 	return
-// }
+func printAge(age1 int, age2 int) (ageOfSally int, ageOfBob int) {
+	ageOfSally = age1
+	ageOfBob = age2
+	return
+}
+
+func printTwoNums(xxx int) (num1 int, num2 int) {
+	return 123, 321
+}
 
 func main() {
-	// 	x, y := printAge(10, 21)
-	// 	fmt.Println(x)
-	// 	fmt.Println(y)
+	x, y := printAge(10, 21)
+	fmt.Println(x)
+	fmt.Println(y)
+
+	fmt.Println(printAge(1110, 213132))
+
+	fmt.Println(printTwoNums(1110213132))
 }
