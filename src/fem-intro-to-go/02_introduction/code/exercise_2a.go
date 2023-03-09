adfeddcb3da1cc5dfd312a0c09171ba951356a33
Mike Rohner <mrohner94@hotmail.com>
2023-03-08 20:21:54 -0500

    2a

---
+++ src/fem-intro-to-go/02_introduction/code/exercise_2a.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// // Note: only uncomment one main() function at a time when testing
+
+// // Part One
+// func main() {
+// 	fmt.Println("Hello World")
+// }
+
+// // Part Two
+func main() {
+	fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing (in the summer), which is %t", "Mike", "Buffalo", 5, true)
+}
