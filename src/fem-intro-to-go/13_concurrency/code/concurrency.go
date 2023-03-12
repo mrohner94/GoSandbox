027efd739d69979ff048dc31eb446bfc8bc297ab
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-03-12 16:18:38 -0400

    channels/concurrency

--- src/fem-intro-to-go/13_concurrency/code/concurrency.go
+++ src/fem-intro-to-go/13_concurrency/code/concurrency.go
@@ -31,10 +31,48 @@ func printStuff(num int) {
 	}
 }
 
+func mathThings(c chan int, someValue int) {
+	defer wg.Done()
+	// From right to left, calculate the value, send it to the channel
+	c <- someValue * 5
+}
+
 func main() {
-	wg.Add(3)
-	go printStuff(40)
-	go printStuff(3)
-	go printStuff(13)
+	// wg.Add(3)
+	// go printStuff(2)
+	// go printStuff(3)
+	// go printStuff(1)
+	// wg.Wait()
+
+	// resultChan := make(chan int)
+
+	// go mathThings(resultChan, 5)
+	// go mathThings(resultChan, 3)
+
+	// num1 := <-resultChan
+	// num2 := <-resultChan
+
+	// fmt.Println(num1, num2)
+
+
+	resultChan := make(chan int, 10)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go mathThings(resultChan, i)
+	}
+
+	// Wait for all goroutines to complete before closing the channel
 	wg.Wait()
+
+	// Close wont wait for the goroutines to be done without additional synchronization
+	close(resultChan)
+
+
+	for item := range resultChan {
+		fmt.Println(item)
+	}
+
+
+
 }
